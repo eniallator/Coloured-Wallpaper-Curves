32a249d4f55f2292166ffb3fc0a3c8e43876f2a2
eniallator <eniallatormc@gmail.com>
2023-01-29 17:02:35 +0000

    Updated the src with the latest template code

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const paramConfig = new ParamConfig(
 );
 paramConfig.addCopyToClipboardHandler("#share-btn");
 
+document.getElementById("download-btn").onclick = function (_evt) {
+  const url = canvas.toDataURL();
+  const anchor = document.createElement("a");
+  anchor.href = url;
+  anchor.download = `${
+    document.getElementsByTagName("title")?.[0].innerText ?? "download"
+  }.png`;
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+};
+
 function hexToRGB(hex) {
   const match = hex
     .toUpperCase()
